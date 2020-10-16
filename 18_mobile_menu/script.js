19ee35d1f0b8febfa8a017819a68020427b6c561
Anh Le <maianh_tom@yahoo.com.vn>
2020-10-16 21:23:36 +0300

    portfolio step 1,2,3,6,7

---
+++ 18_mobile_menu/script.js
@@ -0,0 +1,7 @@
+const mobileMenu = () => {
+    const menu = document.querySelector('ul');
+
+    menu.classList.toggle('mobilemenu');
+}
+
+/*const mobileMenu = () => document.querySelector('').classList.toggle('mobilemenu');*/
